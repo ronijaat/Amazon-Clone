81fe240254ee27ee9a2d4e6f1ac7de0bda1570aa
ronijaat <raunakbaliyan01@gmail.com>
2023-09-26 09:32:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ require('./db/conn');
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
-
 const Products = require("./models/productsSchema");
 const DefaultData = require("./defaultdata");
 const router = require("./routes/router");
@@ -23,9 +22,9 @@ app.use(router);
 const PORT = process.env.PORT || 8005;
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname,"client","build")));
 }
-app.use(express.static("client/build"))
+app.use(express.static(path.join(__dirname,"client","build")));
 
 app.listen(PORT,()=>{
     console.log(`server is running on port number ${PORT}`);
